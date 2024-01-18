47951e8d81aa21a8b7b2d7395e3a46e1b0d31986
roshan <roshanmandal1506@gmail.com>
2024-01-18 15:29:19 +0530

    Redux CRUD

--- Project_18/RTK_CRUD/src/Home.jsx
+++ Project_18/RTK_CRUD/src/Home.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Link , useNavigate } from 'react-router-dom';
+import { deleteUser } from './UserReducer';
 
 function Home() {
     const users = useSelector((state) => state.users);
-    console.log(users);
+    
+    const dispatch = useDispatch();
+    const handleDelete = (id) => {
+      dispatch(deleteUser({id : id}))
+    }
   return (
     <div className='container'>
       <h2> CRUD App with JSON Server</h2>
-      <button className='btn btn-success my-3'>Create +  </button>
+      <Link to="/create" className='btn btn-success my-3'>Create +  </Link>
       <table className='table'>
          <thead> 
            <tr>
@@ -18,6 +24,17 @@ function Home() {
            </tr>
          </thead>
          <tbody>
+           {users.map((user,index) => (
+               <tr key={index}>
+                   <td>{user.id}</td>
+                   <td>{user.name}</td>
+                   <td>{user.email}</td>
+                   <td>
+                      <Link to={`/edit/${user.id}`} className='btn btn-sm btn-primary'>Edit</Link>
+                      <button onClick={() => handleDelete(user.id)} className='btn btn-sm btn-danger ms-2'>Delete</button>
+                   </td>
+               </tr>
+           ))}
 
 
          </tbody>
--- Project_18/RTK_CRUD/src/UserReducer.jsx
+++ Project_18/RTK_CRUD/src/UserReducer.jsx
@@ -6,7 +6,32 @@ const userSlice = createSlice({
     initialState : userList,
     reducers :{
 
+        // add user
+        addUser : ( state , action) => {
+            state.push(action.payload)
+        },
+        
+        // update user
+        updateUser : (state , action) => {
+             const {id , name , email} = action.payload;
+             const uu = state.find(user => user.id == id);
+             if (uu) {
+                uu.name = name;
+                uu.email = email;
+             }
+         },
+
+        // delete user
+        deleteUser : (state , action) => {
+            const {id} = action.payload;
+             const uu = state.find(user => user.id == id);
+             if (uu) {
+                return state.filter(f => f.id !== id)
+             }
+        }
+
     }
 })
 
+export const {addUser , updateUser , deleteUser} = userSlice.actions
 export default userSlice.reducer;
